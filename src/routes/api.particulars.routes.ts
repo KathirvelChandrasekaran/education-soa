9d9278f3590b927e53d0006f3c8d359b0e67ce67
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-10-17 12:55:27 +0530

    update: add details for sem 1 - 6

--- src/routes/api.particulars.routes.ts
+++ src/routes/api.particulars.routes.ts
@@ -164,6 +164,218 @@ router.post(
           completed: true,
         },
       });
+      res.status(200).send({
+        message: "Added successfully!!!",
+      });
+    } catch (error) {
+      res.status(401).send({ message: error });
+    }
+  }
+);
+
+router.post(
+  "/sem1",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { isDayScholar, tutor_name, arrear_count, redo_count, cgpa } =
+        req.body;
+      var user = await prisma.sem1.findUnique({
+        where: {
+          email: res.locals.email,
+        },
+      });
+
+      if (user?.completed)
+        return res.status(403).send({ message: "You have already completed" });
+      await prisma.sem1.update({
+        where: { email: res.locals.email },
+        data: {
+          isDayScholar,
+          tutor_name,
+          arrear_count,
+          redo_count,
+          cgpa,
+          completed: true,
+        },
+      });
+      res.status(200).send({
+        message: "Added successfully!!!",
+      });
+    } catch (error) {
+      res.status(401).send({ message: error });
+    }
+  }
+);
+
+router.post(
+  "/sem2",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { isDayScholar, tutor_name, arrear_count, redo_count, cgpa } =
+        req.body;
+      var user = await prisma.sem2.findUnique({
+        where: {
+          email: res.locals.email,
+        },
+      });
+
+      if (user?.completed)
+        return res.status(403).send({ message: "You have already completed" });
+      await prisma.sem2.update({
+        where: { email: res.locals.email },
+        data: {
+          isDayScholar,
+          tutor_name,
+          arrear_count,
+          redo_count,
+          cgpa,
+          completed: true,
+        },
+      });
+      res.status(200).send({
+        message: "Added successfully!!!",
+      });
+    } catch (error) {
+      res.status(401).send({ message: error });
+    }
+  }
+);
+
+router.post(
+  "/sem3",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { isDayScholar, tutor_name, arrear_count, redo_count, cgpa } =
+        req.body;
+      var user = await prisma.sem3.findUnique({
+        where: {
+          email: res.locals.email,
+        },
+      });
+
+      if (user?.completed)
+        return res.status(403).send({ message: "You have already completed" });
+      await prisma.sem3.update({
+        where: { email: res.locals.email },
+        data: {
+          isDayScholar,
+          tutor_name,
+          arrear_count,
+          redo_count,
+          cgpa,
+          completed: true,
+        },
+      });
+      res.status(200).send({
+        message: "Added successfully!!!",
+      });
+    } catch (error) {
+      res.status(401).send({ message: error });
+    }
+  }
+);
+
+router.post(
+  "/sem4",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { isDayScholar, tutor_name, arrear_count, redo_count, cgpa } =
+        req.body;
+      var user = await prisma.sem4.findUnique({
+        where: {
+          email: res.locals.email,
+        },
+      });
+
+      if (user?.completed)
+        return res.status(403).send({ message: "You have already completed" });
+      await prisma.sem4.update({
+        where: { email: res.locals.email },
+        data: {
+          isDayScholar,
+          tutor_name,
+          arrear_count,
+          redo_count,
+          cgpa,
+          completed: true,
+        },
+      });
+      res.status(200).send({
+        message: "Added successfully!!!",
+      });
+    } catch (error) {
+      res.status(401).send({ message: error });
+    }
+  }
+);
+
+router.post(
+  "/sem5",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { isDayScholar, tutor_name, arrear_count, redo_count, cgpa } =
+        req.body;
+      var user = await prisma.sem5.findUnique({
+        where: {
+          email: res.locals.email,
+        },
+      });
+
+      if (user?.completed)
+        return res.status(403).send({ message: "You have already completed" });
+      await prisma.sem5.update({
+        where: { email: res.locals.email },
+        data: {
+          isDayScholar,
+          tutor_name,
+          arrear_count,
+          redo_count,
+          cgpa,
+          completed: true,
+        },
+      });
+      res.status(200).send({
+        message: "Added successfully!!!",
+      });
+    } catch (error) {
+      res.status(401).send({ message: error });
+    }
+  }
+);
+router.post(
+  "/sem6",
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { isDayScholar, tutor_name, arrear_count, redo_count, cgpa } =
+        req.body;
+      var user = await prisma.sem6.findUnique({
+        where: {
+          email: res.locals.email,
+        },
+      });
+
+      if (user?.completed)
+        return res.status(403).send({ message: "You have already completed" });
+      await prisma.sem6.update({
+        where: { email: res.locals.email },
+        data: {
+          isDayScholar,
+          tutor_name,
+          arrear_count,
+          redo_count,
+          cgpa,
+          completed: true,
+        },
+      });
+      res.status(200).send({
+        message: "Added successfully!!!",
+      });
     } catch (error) {
       res.status(401).send({ message: error });
     }
