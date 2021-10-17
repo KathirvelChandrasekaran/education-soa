5b264a8f40ecb31b9e79a0912a0865e409a8ddf4
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-10-17 22:12:58 +0530

    update: Procfile

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,37 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import express, {
-  Request,
-  Response,
-  NextFunction,
-  Application,
-  ErrorRequestHandler,
-} from "express";
-import { Server } from "http";
-import createHttpError from "http-errors";
-import morgan from "morgan";
-import dotenv from "dotenv";
-
-dotenv.config();
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express, { Request, Response, NextFunction, Application, ErrorRequestHandler } from 'express';
+import { Server } from 'http';
+import createHttpError from 'http-errors';
+import morgan from 'morgan';
 
 const app: Application = express();
 
 app.use(bodyParser.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(cors());
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello');
 });
 
-app.use("/api/v1/auth", require("./routes/api.auth.routes"));
-app.use(
-  "/api/v1/student/particulars",
-  require("./routes/api.particulars.routes")
-);
+app.use('/api/v1/auth', require('./routes/api.auth.routes'));
+app.use('/api/v1/student/particulars', require('./routes/api.particulars.routes'));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   next(new createHttpError.NotFound());
 });
 
-const errorHandler: ErrorRequestHandler = (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const errorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) => {
   res.status(err.status || 500);
   res.send({
     status: err.status || 500,
-    message: err.message || "Server error",
+    message: err.message || 'Server error'
   });
 };
 
 app.use(errorHandler);
 const port = process.env.PORT || 3000;
 
-const server: Server = app.listen({ port }, () =>
-  console.log(`http://localhost:${port}`)
-);
+const server: Server = app.listen({ port }, () => console.log(`http://localhost:${port}`));
