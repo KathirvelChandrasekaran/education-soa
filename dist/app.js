7c6166cae00f2c59c46456ad7eb3bf081c29ae69
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-10-17 22:26:27 +0530

    update: Procfile

--- dist/app.js
+++ dist/app.js
@@ -8,17 +8,15 @@ const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const http_errors_1 = __importDefault(require("http-errors"));
 const morgan_1 = __importDefault(require("morgan"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
-app.use((0, morgan_1.default)("dev"));
+app.use((0, morgan_1.default)('dev'));
 app.use((0, cors_1.default)());
-app.get("/", (req, res, next) => {
-    res.send("Hello");
+app.get('/', (req, res, next) => {
+    res.send('Hello');
 });
-app.use("/api/v1/auth", require("./routes/api.auth.routes"));
-app.use("/api/v1/student/particulars", require("./routes/api.particulars.routes"));
+app.use('/api/v1/auth', require('./routes/api.auth.routes'));
+app.use('/api/v1/student/particulars', require('./routes/api.particulars.routes'));
 app.use((req, res, next) => {
     next(new http_errors_1.default.NotFound());
 });
@@ -26,7 +24,7 @@ const errorHandler = (err, req, res, next) => {
     res.status(err.status || 500);
     res.send({
         status: err.status || 500,
-        message: err.message || "Server error",
+        message: err.message || 'Server error'
     });
 };
 app.use(errorHandler);
